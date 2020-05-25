88678e40d04ed0959446b11319ea748aff052b3f
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-05-25 17:49:32 +0300

    update icon to section-switch

--- src/themes/bootstrap4/views/modules/header/section.php
+++ src/themes/bootstrap4/views/modules/header/section.php
@@ -1,29 +1,28 @@
 <?php
 
 $sections = isset($this->sections) ? $this->sections : false;
-if (!$sections && class_exists('Sections')) {
-    $sections = Sections::instance()->getAll();
-}
 if (!$sections) {
     return;
 }
 
 ?>
 
-<ul class="navbar-nav">
+<ul class="navbar-nav navbar-sections">
     <li class="nav-item dropdown">
         <a class="nav-item nav-link dropdown-toggle mr-md-2 px-2" href="#" id="admin-sections" data-toggle="dropdown"
            aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-th"></i>
         </a>
         <div class="dropdown-menu dropdown-menu-md-left" aria-labelledby="admin-sections">
-
             <?php foreach ($sections as $section) { ?>
-                <?php if ($section->organizers_switch === true) { ?>
-                    <a class="dropdown-item" href="<?php echo $section->getURL(); ?>">
-                        <?php echo $section->getName(); ?>
-                    </a>
-                <?php } ?>
+                <a class="dropdown-item" target="_blank"
+                   href="<?php echo $section->getURL(\Nip\Router\route(request()->getModuleName())); ?>"
+                >
+                    <span class="icon" style="<?php echo ($section->color) ? 'background-color:'.$section->color : ''; ?>">
+                        <?php echo $section->printIcon(); ?>
+                    </span>
+                    <?php echo $section->getName(); ?>
+                </a>
             <?php } ?>
         </div>
     </li>
