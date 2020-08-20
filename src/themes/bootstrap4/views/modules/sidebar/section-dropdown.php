20782ba5d8871330e193b2cb040de43a604da9ad
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-08-20 15:41:25 +0300

    update sidebar

--- src/themes/bootstrap4/views/modules/sidebar/section-dropdown.php
+++ src/themes/bootstrap4/views/modules/sidebar/section-dropdown.php
@@ -2,25 +2,27 @@
 $name = isset($name) ? $name : '';
 $items = isset($items) ? $items : [];
 ?>
-<li class="nav-header" style="border-top:1px solid #fff; background: rgba(0,0,0,0.4)">
-    <div class="dropdown">
-        <?php echo $this->_event->getName(); ?>
+<li class="nav-header has-sub" style=" ">
 
-        <button type="button" class="btn btn-default btn-xs dropdown-toggle text-white" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            &nbsp;
-        </button>
-        <div class="dropdown-menu" role="menu">
-            <?php if (count($items)) { ?>
-                <?php foreach ($items as $item) { ?>
-                    <?php
-                    $url = is_object($item) ? $item->getURL() : '';
-                    $name = is_object($item) ? $item->getName() : '';
-                    ?>
-                    <a class="dropdown-item" href="<?php echo $url; ?>">
+    <a href="javascript:"
+       data-toggle="collapse"  role="button" aria-expanded="false" aria-controls="navHeaderDropdown" style="font-size: 85%">
+        <b class="caret pull-right"></b>
+        <?php echo $name; ?>
+    </a>
+
+    <ul class="sub-menu" role="menu">
+        <?php if (count($items)) { ?>
+            <?php foreach ($items as $item) { ?>
+                <?php
+                $url = is_object($item) ? $item->getURL() : '';
+                $name = is_object($item) ? $item->getName() : '';
+                ?>
+                <li class="">
+                    <a class="" href="<?php echo $url; ?>">
                         <?php echo $name; ?>
                     </a>
-                <?php } ?>
+                </li>
             <?php } ?>
-        </div>
-    </div>
+        <?php } ?>
+    </ul>
 </li>
