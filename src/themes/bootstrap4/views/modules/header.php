6560108042c1a7dabb785d62f604d1fc660396e8
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-08-20 11:43:55 +0300

    update admin navbar

--- src/themes/bootstrap4/views/modules/header.php
+++ src/themes/bootstrap4/views/modules/header.php
@@ -1,7 +1,4 @@
 <?php
-
-use Nip\Records\Locator\ModelLocator;
-
 $currentSection = mvc_sections()->getCurrent();
 ?>
 <!-- begin #header -->
@@ -23,35 +20,7 @@ $currentSection = mvc_sections()->getCurrent();
     </div>
     <!-- end navbar-header -->
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#userHeaderNavbar"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <?php echo $this->loadIfExists('/modules/header/menu'); ?>
 
-    <!-- begin header-nav -->
-
-    <div class="collapse navbar-collapse" id="userHeaderNavbar">
-        <ul class="navbar-nav ml-md-auto">
-            <li class="dropdown navbar-user">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                    <i class="icon-user icon-white"></i>
-                    <span class="d-none d-md-inline"><?php echo $this->user->getName(); ?></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a href="<?php echo ModelLocator::get('Administrators')->compileURL('ChangePassword'); ?>"
-                       class="dropdown-item"
-                       title="<?php echo ModelLocator::get('Users')->getLabel('password.change'); ?>">
-                        <?php echo ModelLocator::get('Users')->getLabel('password.change'); ?>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="<?php echo ModelLocator::get('Administrators')->compileURL('Logout'); ?>"
-                       title="Logout"
-                       class="dropdown-item">
-                        <i class="icon-off"></i>
-                        Logout
-                    </a>
-                </div>
-            </li>
-        </ul>
-    </div>
+    <?php echo $this->loadIfExists('/modules/header/user-menu'); ?>
 </header>
---
+++ src/themes/bootstrap4/views/modules/header/user-menu.php
@@ -0,0 +1,37 @@
+<?php
+
+$currentUser = $this->user;
+$currentUserManager = $this->user->getManager();
+?>
+
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#userHeaderNavbar"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<!-- begin header-nav -->
+
+<div class="collapse navbar-collapse" id="userHeaderNavbar">
+    <ul class="navbar-nav ml-md-auto">
+        <li class="nav-item dropdown navbar-user">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                <i class="icon-user icon-white"></i>
+                <span class="d-none d-md-inline"><?php echo $this->user->getName(); ?></span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <a href="<?php echo $currentUserManager->compileURL('ChangePassword'); ?>"
+                   class="dropdown-item"
+                   title="<?php echo $currentUserManager->getLabel('password.change'); ?>">
+                    <?php echo $currentUserManager->getLabel('password.change'); ?>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="<?php echo $currentUserManager->compileURL('Logout'); ?>"
+                   title="Logout"
+                   class="dropdown-item">
+                    <i class="icon-off"></i>
+                    Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</div>
