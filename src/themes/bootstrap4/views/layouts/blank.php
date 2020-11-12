524411dec58dda2679874ef6e796225a798f663c
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-11-12 18:05:35 +0200

    add bootstrap4 add blank layout

---
+++ src/themes/bootstrap4/views/layouts/blank.php
@@ -0,0 +1,7 @@
+<?php echo $this->Doctype(); ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php echo $this->load("/modules/head"); ?>
+<body>
+<?php echo $this->render("content"); ?>
+</body>
+</html>
