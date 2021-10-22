53eff7e52bdb90c6529a441f06b648f338fc5418
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-10-22 19:51:16 +0300

    remove notice

--- src/themes/bootstrap5/views/modules/sidebar/items.php
+++ src/themes/bootstrap5/views/modules/sidebar/items.php
@@ -1,35 +1,38 @@
 <?php
-$expanded = isset($expanded) ? $expanded : null;
-$selected = isset($selected) ? $selected : null;
+/** @var array $menu */
+$expanded = $expanded ?? null;
+$selected = $selected ?? null;
 ?>
 <?php foreach ($menu as $section => $item) { ?>
-    <?php if ($item['enabled']) { ?>
-        <?php $hasSubMenu = isset($item['submenus']) && is_array($item['submenus']) && count($item['submenus']) > 0; ?>
-        <?php $sectionSelected = (isset($item['active']) && $item['active']) or ($section === $expanded); ?>
-        <li class="<?php echo $sectionSelected ? ' active' : ''; ?><?php echo $hasSubMenu ? ' has-sub' : ''; ?>">
-            <a href="<?php echo $item['href'] ? $item['href'] : 'javascript:'; ?>"
-               class="<?php echo isset($item['class']) ? $item['class'] : ''; ?>"
-                <?php echo isset($item['attributes']) ? $this->HTML()->attributes($item['attributes']) : ''; ?>
-            >
-                <?php echo $hasSubMenu ? '<b class="caret pull-right"></b>' : ''; ?>
+    <?php if (isset($item['enabled']) && $item['enabled'] === false) { ?>
+        <?php continue; ?>
+    <?php } ?>
+    <?php $hasSubMenu = isset($item['submenus']) && is_array($item['submenus']) && count($item['submenus']) > 0; ?>
+    <?php $sectionSelected = (isset($item['active']) && $item['active']) or ($section === $expanded); ?>
+    <li class="<?= $sectionSelected ? ' active' : ''; ?><?php echo $hasSubMenu ? ' has-sub' : ''; ?>">
+        <a href="<?= $item['href'] ? $item['href'] : 'javascript:'; ?>"
+           class="<?= isset($item['class']) ? $item['class'] : ''; ?>"
+            <?= isset($item['attributes']) ? $this->HTML()->attributes($item['attributes']) : ''; ?>
+        >
+            <?php echo $hasSubMenu ? '<b class="caret pull-right"></b>' : ''; ?>
 
-                <?php
-                if (isset($item['icon']) && !empty($item['icon'])) {
-                    $iconType = false;
-                    if (strpos($item['icon'], 'glyphicon') === 0) {
-                        $iconType = 'glyphicon';
-                    } elseif (strpos($item['icon'], 'fa-') === 0) {
-                        $iconType = 'fa';
-                    }
-                    $classPrefix = '';
-                    switch ($iconType) {
-                        case 'glyphicon':
-                            $classPrefix = 'glyphicon';
-                            break;
-                        case 'fa':
-                            $classPrefix = 'fa';
-                            break;
-                    }
+            <?php
+            if (isset($item['icon']) && !empty($item['icon'])) {
+                $iconType = false;
+                if (strpos($item['icon'], 'glyphicon') === 0) {
+                    $iconType = 'glyphicon';
+                } elseif (strpos($item['icon'], 'fa-') === 0) {
+                    $iconType = 'fa';
+                }
+                $classPrefix = '';
+                switch ($iconType) {
+                    case 'glyphicon':
+                        $classPrefix = 'glyphicon';
+                        break;
+                    case 'fa':
+                        $classPrefix = 'fa';
+                        break;
+                }
                     echo '<i class="' . $classPrefix . ' ' . $item['icon'] . ($sectionSelected ? ' glyphglyphicon-white' : '') . '"></i>';
                 } else {
                     echo '<i class="fa fa-th-large"></i>';
@@ -43,17 +46,16 @@ $selected = isset($selected) ? $selected : null;
                 <ul class="sub-menu" style="display:<?php echo $sectionSelected ? 'block' : 'none'; ?>;">
                     <?php foreach ($item['submenus'] as $subsection => $sub) { ?>
                         <li <?php echo $subsection == $selected ? ' class="active"' : ''; ?>>
-                            <a href="<?php echo $sub['href']; ?>"
-                               title="<?php echo $sub['name']; ?>"
+                            <a href="<?= $sub['href']; ?>"
+                               title="<?= $sub['name']; ?>"
                                 <?php echo isset($item['attributes']) ? $this->HTML()->attributes($sub['attributes']) : ''; ?>
                             >
                                 <?php echo isset($sub['icon']) ? '<span class="glyphicon ' . $sub['icon'] . '"></span>' : ''; ?>
-                                <?php echo $sub['name']; ?>
+                                <?= $sub['name']; ?>
                             </a>
                         </li>
                     <?php } ?>
                 </ul>
             <?php } ?>
         </li>
-    <?php } ?>
 <?php } ?>  
