e5008de213431613629954b56525c1014e8721b1
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-03-26 22:44:17 +0200

    feat: add migration state service and update jQuery UI autocomplete widget

--- resources/themes/abstract/assets/scripts/jquery-ui.js
+++ resources/themes/abstract/assets/scripts/jquery-ui.js
@@ -1,27 +1,33 @@
-import 'jquery-ui/ui/widgets/sortable.js';
+import $ from 'jquery';
+import 'jquery-ui/ui/widget';
+import 'jquery-ui/ui/widgets/menu';
+import 'jquery-ui/ui/widgets/sortable';
 import 'jquery-ui/ui/widgets/datepicker';
-import 'jquery-ui/ui/widgets/autocomplete.js';
+import 'jquery-ui/ui/widgets/autocomplete';
 
-// document.addEventListener("DOMContentLoaded", function() {
-    $.widget("custom.catcomplete", $.ui.autocomplete, {
+const autocompleteWidget = $.ui && $.ui.autocomplete;
+
+// Register only when jQuery UI autocomplete is available to avoid hard runtime crashes.
+if (autocompleteWidget) {
+    $.widget('custom.catcomplete', autocompleteWidget, {
         _create: function () {
             this._super();
-            this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
+            this.widget().menu('option', 'items', '> :not(.ui-autocomplete-category)');
         },
         _renderMenu: function (ul, items) {
             var that = this,
-                currentCategory = "";
+                currentCategory = '';
             $.each(items, function (index, item) {
                 var li;
                 if (item.category != currentCategory) {
-                    ul.append("<li class='ui-autocomplete-category' style='border-bottom:1px solid #cacaca'><strong>" + item.category + "</strong></li>");
+                    ul.append("<li class='ui-autocomplete-category' style='border-bottom:1px solid #cacaca'><strong>" + item.category + '</strong></li>');
                     currentCategory = item.category;
                 }
                 li = that._renderItemData(ul, item);
                 if (item.category) {
-                    li.attr("aria-label", item.category + " : " + item.label);
+                    li.attr('aria-label', item.category + ' : ' + item.label);
                 }
             });
         }
     });
-// });
+}
